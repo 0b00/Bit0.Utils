f3c3b91b060d20cfa2b7d84ea2961ad51d87c3b8
Ishan Jain <ishan@jain.se>
2017-01-25 01:03:06 -0800

    Added mapper and data interfaces

---
+++ src/Common/Interfaces/IData.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Bit0.Utils.Common
+{
+    /// <summary>
+    /// Entity object
+    /// </summary>
+    public interface IData
+    {
+        /// <summary>
+        /// <see cref="IData"/> Id
+        /// </summary>
+        string Id { get; set; }
+    }
+}
