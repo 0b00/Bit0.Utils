84fcc81f960522fe3d996de1e839d06cbeae2968
Ishan Jain <ishan@jain.se>
2017-02-25 10:05:06 -0800

    added string comparer and compare extension

---
+++ src/Common/Comparer/StringContainsComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bit0.Utils.Common.Comparer
+{
+    /// <summary>
+    /// String comparer for Linq
+    /// </summary>
+    public class StringContainsComparer : IEqualityComparer<string>
+    {
+        /// <summary>
+        /// Check if string are equal, ignoring case
+        /// </summary>
+        /// <param name="str1">String 1</param>
+        /// <param name="str2">String 2</param>
+        /// <returns>Tre if str1 and str2 are equal, else false</returns>
+        public bool Equals(string str1, string str2)
+        {
+            return str1.ToLowerInvariant().Equals(str2.ToLowerInvariant(), StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Returns the hah code of string
+        /// </summary>
+        /// <param name="str">String to get hash code of</param>
+        /// <returns>String hashcode</returns>
+        public int GetHashCode(string str)
+        {
+            return str.GetHashCode();
+        }
+    }
+}
--- src/Common/Extensions/StringExtensions.cs
+++ src/Common/Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Bit0.Utils.Common.Attributes;
+using System.Linq;
 
 namespace Bit0.Utils.Common.Extensions
 {
@@ -16,6 +18,24 @@ namespace Bit0.Utils.Common.Extensions
         public static string GetValue(this Enum s)
         {
             return s.GetFieldAttribute<StringAttribute>().Value;
+        }
+
+        /// <summary>
+        /// Determines whether the specified list contains the matching string value
+        /// 
+        /// Snippet from: http://stackoverflow.com/a/10254081
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <param name="value">The value to match.</param>
+        /// <param name="ignoreCase">if set to <c>true</c> the case is ignored.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified list contais the matching string; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Contains(this IEnumerable<string> list, string value, bool ignoreCase = false)
+        {
+            return ignoreCase ?
+                list.Any(s => s.Equals(value, StringComparison.OrdinalIgnoreCase)) :
+                list.Contains(value);
         }
     }
 }
