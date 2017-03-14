70c1aac906895e55697ec51c7a46a9d78f5fc5b7
Ishan Jain <ishan@jain.se>
2017-03-14 18:03:54 +0100

    start with jwt

---
+++ src/Security/Jwt/JwtToken.cs
@@ -0,0 +1,56 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Bit0.Utils.Security.Jwt
+{
+    /// <summary>
+    /// JSON Web Token
+    /// </summary>
+    public class JwtToken
+    {
+        /// <summary>
+        /// Generate new token
+        /// </summary>
+        /// <param name="claimsIdentity">Claims in the token</param>
+        /// <param name="signingKey">Key</param>
+        /// <returns></returns>
+        public static string Generate(ClaimsIdentity claimsIdentity, SecurityKey signingKey)
+        {
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var securityTokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = claimsIdentity,
+                SigningCredentials = signingCredentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var plainToken = tokenHandler.CreateToken(securityTokenDescriptor);
+            var signedAndEncodedToken = tokenHandler.WriteToken(plainToken);
+
+            return signedAndEncodedToken;
+        }
+
+        /// <summary>
+        /// Validate a JWT token
+        /// </summary>
+        /// <param name="signedAndEncodedToken">TOken</param>
+        /// <param name="signingKey">Key</param>
+        /// <returns></returns>
+        public SecurityToken Validate(string signedAndEncodedToken, SecurityKey signingKey)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+                IssuerSigningKey = signingKey
+            };
+
+            SecurityToken validatedToken;
+            tokenHandler.ValidateToken(signedAndEncodedToken, tokenValidationParameters, out validatedToken);
+
+            return validatedToken;
+        }
+    }
+}
---
+++ src/Security/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Security.Jwt")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3785ed6-babf-4e3d-9e99-8a699aa26163")]
