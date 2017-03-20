d5abf3d46747fe75146adaf519fb55802296b983
b1thunt3r <ishan@jain.se>
2017-03-20 22:33:34 +0100

    Move InvalidDataCastExp to JSend. Update nugets...

--- src/JSend/Exceptions/InvalidDataCastException.cs
+++ src/JSend/Exceptions/InvalidDataCastException.cs
@@ -13,10 +13,10 @@ namespace Bit0.Utils.Common.Data
         /// <summary>
         /// Invalid <see cref="IData"/> cast exception
         /// </summary>
-        /// <param name="id">Original id</param>
+        /// <param name="from">Original type</param>
         /// <param name="inner">Inner exception</param>
-        public InvalidDataCastException(string id, Exception inner = null)
-            : base($"Cannot convert '{id}' to {typeof(IData).Name}", 500, inner)
+        public InvalidDataCastException(string from, Exception inner = null)
+            : base($"Cannot convert '{from}' to {typeof(IData).Name}", 500, inner)
         { }
     }
 }
---
+++ src/JSend/Exceptions/JsendNotImplemented.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bit0.Utils.JSend.Exceptions
+{
+    /// <summary>
+    /// Not Implemented
+    /// </summary>
+    public class JsendNotImplemented : JSendException
+    {
+        /// <summary>
+        /// Not Implemented
+        /// </summary>
+        /// <param name="message">Error message</param>
+        /// <param name="inner">Inner exception</param>
+        protected JsendNotImplemented(string message, Exception inner = null) 
+            : base(message, 500, inner)
+        { }
+    }
+}
