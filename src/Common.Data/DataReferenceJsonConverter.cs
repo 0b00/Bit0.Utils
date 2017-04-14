9785e67dc9fb980c860a30ed84f3c0354a28dfec
b1thunt3r <ishan@jain.se>
2017-04-14 23:04:38 +0200

    add comments

--- src/Common.Data/DataReferenceJsonConverter.cs
+++ src/Common.Data/DataReferenceJsonConverter.cs
@@ -3,15 +3,20 @@ using Newtonsoft.Json;
 
 namespace Bit0.Utils.Common.Data
 {
+    /// <summary>
+    /// <see cref="JsonConverter"/> for <see cref="DataReference"/>.
+    /// Serialize only the Id as string.
+    /// </summary>
     public class DataReferenceJsonConverter : JsonConverter
     {
         #region Public Methods
-
+        /// <inheritdoc />
         public override bool CanConvert(Type objectType)
         {
             return objectType == typeof(DataReference);
         }
 
+        /// <inheritdoc />
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var str = (string)reader.Value;
@@ -19,6 +24,7 @@ namespace Bit0.Utils.Common.Data
             return DataReference.Parse(str);
         }
 
+        /// <inheritdoc />
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             writer.WriteRawValue($"\"{((DataReference)value).Id}\"");
