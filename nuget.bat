0b73dd159bac1e3b88d38ac90490e64fddf0ce66
Ishan Jain <ishan.jain@avensia.com>
2017-09-07 14:56:28 +0200

    start replacing nuget referemces with project

--- nuget.bat
+++ nuget.bat
@@ -1,14 +1,14 @@
 echo off
-echo ^> Create Nuget package for %1 (%2) in %nugetDir%
+echo ## Create Nuget package for %1 (%2) in %nugetDir%
 
-echo ^> Create packages
+::echo ## Create packages
 dotnet pack %1 --no-build --configuration %2 --output %nugetDir% --include-symbols --verbosity minimal
 
-echo ^> Check if Nuget Symbols directory exists, otherwise create one
+::echo ## Check if Nuget Symbols directory exists, otherwise create one
 set nugetSymbolsDir=%nugetDir%\symbols
 
-echo ^> Move Nuget Symbols, to Symbols directory
+::echo ## Move Nuget Symbols, to Symbols directory
 IF NOT EXIST %nugetSymbolsDir% mkdir %nugetSymbolsDir%
 move %nugetDir%\*.symbols.nupkg %nugetSymbolsDir%
 
-echo ^> Complete
+::echo ## Complete
